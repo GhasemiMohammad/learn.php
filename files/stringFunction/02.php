855ffd8f8f33438fd1441f96da27eee887c77ac2
GhasemiMohammad <mohammadghasemi824@gmail.com>
2023-08-01 11:05:07 +0330

    add scond string function file

---
+++ files/stringFunction/02.php
@@ -0,0 +1,7 @@
+<?php    
+    /* -------------------------------*/
+    echo "this is nl2br function : "."<br>";
+    echo "without nl2br :  this is one line \n this is another line"."<br>";
+    echo "with nl2br : ". nl2br("without nl2br :  this is one line \n this is another line");
+    /* -------------------------------*/
+    echo "<hr>";
