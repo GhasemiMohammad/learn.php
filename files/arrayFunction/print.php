94ac4654cc819f005e228b933e52ff9072aef3bd
GhasemiMohammad <mohammadghasemi824@gmail.com>
2023-08-06 14:47:24 +0330

    try for some function

--- files/arrayFunction/print.php
+++ files/arrayFunction/print.php
@@ -1,7 +1,7 @@
 <?php
     $names=array("Shervin","Shahrzad","Aria","Roodabe");
     $user=array("name"=>"Giti","family"=>"Iran Nejad","age"=>22,"gender"=>"female");
-    $numbers=[1,20,15,105,18,17,19,24,79,83,169,85,-15];
+    $numbers=[1,20,15,105,18,17,19,24,79,83,169,85,-15,20];
     function printArray($value){
         echo "<pre>";
             print_r($value);
--- files/arrayFunction/utilityFunction1.php
+++ files/arrayFunction/utilityFunction1.php
@@ -1,5 +1,8 @@
 <?php
     include("print.php");
+    $rgb=["r"=>"red","g"=>"green","b"=>"blue"];
+    $cmyk=["c"=>"cyan","m"=>"magenta","y"=>"yellow","k"=>"black"];
+    $favColor=["y"=>"yellow","k"=>"black","g"=>"green","c"=>"cyan","b"=>"black"];
     printArray($numbers);
     /* -------------------------------*/
     echo "max ".br();
@@ -10,3 +13,33 @@
     /* -------------------------------*/
     echo "end ".br();
     printValue(end($numbers));
+    /* -------------------------------*/
+    echo "array sum ".br();
+    printValue(array_sum($numbers));
+    /* -------------------------------*/
+    echo "array rand ".br();
+    printValue(array_rand($user));
+    /* -------------------------------*/
+    echo "array chunk ".br();
+    printArray(array_chunk($user,2));
+    /* -------------------------------*/
+    echo "array reverse ".br();
+    printArray(array_reverse($user));
+    /* -------------------------------*/
+    echo "array search ".br();
+    printArray(array_search(-15,$numbers));
+    /* -------------------------------*/
+    echo "array merge ".br();
+    printArray(array_merge($rgb,$cmyk));
+    /* -------------------------------*/
+    echo "array unique ".br();
+    printArray(array_unique($favColor));
+    /* -------------------------------*/
+    echo "array difference ".br();
+    printArray(array_diff_key($favColor,$rgb));
+    /* -------------------------------*/
+    echo "array slice ".br();
+    print_r($user);
+    printArray(array_slice($user,1));
+    
+
