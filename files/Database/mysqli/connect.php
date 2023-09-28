2b1664a8a7261b05e524cd7b0f76fafd410df38d
GhasemiMohammad <mohammadghasemi824@gmail.com>
2023-09-28 12:42:10 +0330

    insert  random user to DB with 2 method

--- files/Database/mysqli/connect.php
+++ files/Database/mysqli/connect.php
@@ -2,6 +2,7 @@
 $mySqlI = new mysqli("localhost", "root", "", "world");
 if ($mySqlI->connect_errno) {
     echo "failed connected to database" . $mySqlI->connect_error . PHP_EOL;
+    exit();
 } else
     echo "connected to database" . PHP_EOL;
 $mySqlI->set_charset("utf8");
